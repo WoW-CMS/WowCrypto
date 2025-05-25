42d14650d9496ae12fc524b4ba0db072fd0e2202
sayghteight <sayghteight@gmail.com>
2025-05-25 02:38:56 +0200

    refactor(SRP6Encryptor): update SRP6 encryption logic to use v2 by default

--- src/Encryptors/SRP6Encryptor.php
+++ src/Encryptors/SRP6Encryptor.php
@@ -32,12 +32,12 @@ class SRP6Encryptor implements WoWEncryptorInterface
     /**
      * SRP6Encryptor constructor
      *
-     * @param bool $isV2 Whether to use SRP6 version 2 (default: false)
+     * @param bool $isV2 Whether to use SRP6 version 2 (default: true)
      */
-    public function __construct(bool $isV2 = false)
+    public function __construct(bool $isV2 = true)
     {
         $this->isV2 = $isV2;
-        $this->N = '894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7';
+        $this->N = '894B645E89E1535BBDAD5B8B290650530801B18EBFBF5E8FAB3C82872A3E9BB7'; // This is for v1SRP
     }
 
     /**
@@ -49,28 +49,31 @@ class SRP6Encryptor implements WoWEncryptorInterface
      */
     public function encrypt(string $username, string $password): array
     {
-        if ($this->isV2) {
-            $salt = random_bytes(32);
-            $g    = gmp_init(7);
-            $N    = gmp_init($this->N, 16);
+        $salt = random_bytes(32);
+        $g    = gmp_init(7);
+        $N    = gmp_init(
+            'AC6BDB41324A9A9BF166DE5E1389582FAF72B6651987EE07FC3192943DB56050A37329CBB4A099ED8193E0757767A13DD52312AB4B0331
+            0DCD7F48A9DA04FD50E8083969EDB767B0CF6095179A163AB3661A05FBD5FAAAE82918A9962F0B93B855F97993EC975EEAA80D740ADBF4F
+            F747359D041D5C33EA71D281E446B14773BCA97B43A23FB801676BD207A436C6481F1D2B9078717461A5B9D32E688F87748544523B524B0
+            D57D5EA77A2775D2ECFA032CFBDBF52FB3786160279004E57AE6AF874E7303CE53299CCC041C7BC308D82A5698F3A8D0C38271AE35F8E9
+            DBFBB694B5C803D89F7AE435DE236D525F54759B65E372FCD68EF20FA7111F9E4AFF73',
+            16
+        );
 
-            $h1 = sha1(strtoupper($username. ':'. $password), true);
-            $h2 = sha1($salt.$h1, true);
-            
-            $h2 = gmp_import($h2, 1, GMP_LSW_FIRST);
+        $password = hash('sha256', strtoupper($username . ':' . $password), true);
+        $x = hash('sha256', $salt. $password, true);
+        
+        // G^h2 mod N
+        $verifier = gmp_powm($g, $x, $N);
 
-            $verifier = gmp_powm($g, $h2, $N);
-            $verifier = gmp_export($verifier, 1, GMP_LSW_FIRST);
-            $verifier = str_pad($verifier, 32, chr(0), STR_PAD_RIGHT);
+        // convert back to a byte array (little-endian)
+        $verifier = gmp_export($verifier, 1, GMP_LSW_FIRST);
+        $verifier = str_pad($verifier, 256, chr(0), STR_PAD_RIGHT);
 
-            return [
-                'verifier' => $verifier,
-               'salt'     => $salt,
-            ];
-        }
-
-        $h1 = sha1(strtoupper($username . ':' . $password), true);
-        return ['hash' => strtoupper(bin2hex($h1)), 'salt' => ''];
+        return [
+            'verifier' => $verifier,
+            'salt'     => $salt,
+        ];
     }
 
     /**
